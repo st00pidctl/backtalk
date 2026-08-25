3b7675a26483927c4ef27802bcc32db967823198
st00pidctl <cassady.samuel95@gmail.com>
2026-08-25 17:27:44 -0600

    Cache versioned endpoint assets

--- endpoint/sw.js
+++ endpoint/sw.js
@@ -1,5 +1,5 @@
 const CACHE = "universal-agent-endpoint-v2";
-const STATIC = ["/", "/styles.css", "/app.js", "/manifest.webmanifest"];
+const STATIC = ["/", "/styles.css?v=2", "/app.js?v=2", "/manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(STATIC)));
